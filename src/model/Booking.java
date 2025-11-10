b41544230a83df41efcf8c9a9a621e227df3cc02
Daniil <daniilgricaev805@gmail.com>
2025-11-10 21:16:18 +0700

    Add Hotel and Booking models

---
+++ src/model/Booking.java
@@ -0,0 +1,46 @@
+package model;
+import java.time.LocalDate;
+
+
+public class Booking {
+    int id;
+    int userID;
+    int hotelID;
+    int roomID;
+    LocalDate startDate;
+    LocalDate endDate;
+    BookingStatus status;
+
+    public Booking(int id,int userID,int hotelID,int roomID,LocalDate startDate,LocalDate endDate){
+        this.id = id;
+        this.userID = userID;
+        this.hotelID = hotelID;
+        this.roomID = roomID;
+        this.startDate = startDate;
+        this.endDate = endDate;
+    }
+    public int getID(){
+        return id;
+    }
+    public int getUserID(){
+        return userID;
+    }
+    public int getHotelID(){
+        return hotelID;
+    }
+    public int getRoomID(){
+        return roomID;
+    }
+    public LocalDate getStartDate(){
+        return startDate;
+    }
+    public LocalDate getEndDate(){
+        return endDate;
+    }
+    public BookingStatus getStatus(){
+        return status;
+    }
+    public void serStatus(BookingStatus status){
+        this.status = status;
+    }
+}
